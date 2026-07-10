a4364d7203d5dad07b8475de54b09b89a8aa4227
charan <charanpersoa@gmail.com>
2026-07-10 12:27:17 +0530

    our first basic user interface of this project

--- learning/finance_project.py
+++ learning/finance_project.py
@@ -1,3 +1,44 @@
 # project for my resume
 # i will include file handling exception handling oops concepts and many libraries here in this project
-open("README.md","w")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os as o
+import random as rd
+import time as t
+
+# first interface that user will se when the open
+class interface():
+    def __init__(self):
+        print("====================================")
+        print(" Personal Finance Analytics System")
+        print("====================================")
+        print("1.REGISTER ")
+        print("2.LOGIN")
+        print("3.EXIT")
+        print("====================================")
+        # options=[1,2,3]
+        user_input=int(input("enter your choice :"))
+        # for val in options:
+        if user_input==1:
+            print("you opted to register:")
+            print("please follow the below instructions:")
+        elif user_input==2:
+            print("you have opted to login :")
+            print("enter your id and password :")
+        elif user_input==3:
+            print("you have opted exit ")
+            print("THANK YOU")
+
+i=interface()
+
+
+        
+# print("====================================")
+# print(" Personal Finance Analytics System")
+# print("====================================")
+# print("REGISTER ")
+# print("LOGIN")
+# print("EXIT")
+# options=["REGISTER ","LOGIN","EXIT"]
+# user_input=int(input("enter your choice :"))
