bd1f9adb6a7ba1021e50682bf6e9fa6cf4f8a613
charan <charanpersoa@gmail.com>
2026-07-10 18:37:21 +0530

    "file structure complete"

--- finance_project_/finance_project.py
+++ finance_project_/finance_project.py
@@ -1,11 +1,10 @@
 # project for my resume
 # i will include file handling exception handling oops concepts and many libraries here in this project
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import os as o
+# import pandas as pd
+# import os as o
 import random as rd
-import time as t
+# import time as t
 
 # first interface that user will se when the open
 class interface():
@@ -19,13 +18,27 @@ class interface():
         print("====================================")
         # options=[1,2,3]
         user_input=int(input("enter your choice :"))
-        # for val in options:
+        id_password={}
+       
+
         if user_input==1:
             print("you opted to register:")
             print("please follow the below instructions:")
+            name=input("enter your name :-")
+            age=input("enter your age :-")
+            gmail=input("enter your email id :-")
+            id=np.rd.randint(0,9,size=10)
+            password=input("set a new strong password :-")
+            id_password[id]=password
+            print("-----------------------------------")
+            print("register process complete you can proceed further .")
+
         elif user_input==2:
             print("you have opted to login :")
             print("enter your id and password :")
+            id=int(input("enter your id :-"))
+            password=input("enter your password :-")
+            print("login successful you can proceed ..")
         elif user_input==3:
             print("you have opted exit ")
             print("THANK YOU")
@@ -33,12 +46,7 @@ class interface():
 i=interface()
 
 
+
+
         
-# print("====================================")
-# print(" Personal Finance Analytics System")
-# print("====================================")
-# print("REGISTER ")
-# print("LOGIN")
-# print("EXIT")
-# options=["REGISTER ","LOGIN","EXIT"]
-# user_input=int(input("enter your choice :"))
+
