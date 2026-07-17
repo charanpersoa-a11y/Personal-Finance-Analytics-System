24e0eee56da090402cc67534e37421c783d60652
charan <charanpersoa@gmail.com>
2026-07-17 14:07:56 +0530

    started the transaction phase

---
+++ finance_project_/mypkg/models/transaction.py
@@ -0,0 +1,14 @@
+# this is the most important thing right now
+# here all the transactions of the user will happen
+# main structure of this transaction is
+# Transaction
+# ├── transaction_id
+# ├── amount
+# ├── date
+# ├── description
+# this is the structure of the transaction\
+import time
+
+print(time.ctime())
+time.sleep(2)
+print(time.strftime("%Y-%m-%d %H:%M:%S"))
--- finance_project_/mypkg/services/dashboard.py
+++ finance_project_/mypkg/services/dashboard.py
@@ -1,10 +1,4 @@
-# this is where the registered or logged in user's dashboard appears
-# and after registration user must login for further operations in this project so main thing is i want to fetch the logged in users information to here
-# how i will know or how this file will know that a person or a user logged in
-# one way is to get a login info here means i the login things will proceed here and directly the info will go to next block of code 
-# other way is get the user id while he is logging in and search him in the data base and then get his information from data base
-# what this dashboard contains
-# 1.the users info like their name email address and all the other transactions in the future
+
 import mypkg.services.sessions as s
 import mypkg.services.auth as a
 import mypkg.services.file_manager as f
