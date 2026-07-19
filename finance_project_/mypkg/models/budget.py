ffadaae411b8ca218c4fb37b3902cca9489975fa
charan <charanpersoa@gmail.com>
2026-07-19 08:57:51 +0530

    budget in progress

--- finance_project_/mypkg/models/budget.py
+++ finance_project_/mypkg/models/budget.py
@@ -1,6 +1,7 @@
 # this is the place where all the budget things happen
 import mypkg.services.file_manager as F
 import mypkg.services.sessions as S
+
 class Budgets:
     def SetBudget(self,category,amount,limit):
         self.category=category
@@ -19,10 +20,30 @@ class Budgets:
 
 
     def GetAll_Budgets():
+        # users and transaction
+        users=F.load_users()
         current_user=S.get_current_user()
+        current_user_data=users[current_user]
+        # budget
         All_Budget=F.LoadBudget()
+
+        print("==============================================================")
+        print(f"NAME: {current_user_data["name"]}")
+        print(f"AGE: {current_user_data["age"]}")
+        print(f"EMAIL ADDRESS : {current_user_data["email"]}")
+        print("==============================================================")
+        print("your transaction history ")
+        trans=F.load_transaction()
+        users_trans=trans[current_user]
+        print(f"YOUR TRANSACTION HISTORY IS {users_trans}")
+        print("==============================================================")
+        print(f"your budget history ")
         if current_user in All_Budget:
-            return All_Budget[current_user]
+            dashboard= All_Budget[current_user]
+            print(f"your budget history is {dashboard}")
+        else:
+            print("currently you don't have and budget created yet ")
+
 
 
 
