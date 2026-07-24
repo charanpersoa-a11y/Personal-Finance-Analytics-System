6d4e8580a5b211794c79fc9da117d67ceb60fcf9
charan <charanpersoa@gmail.com>
2026-07-24 11:20:22 +0530

    analysis initial things done

--- finance_project_/mypkg/services/analytics.py
+++ finance_project_/mypkg/services/analytics.py
@@ -4,6 +4,7 @@
 # the first whenever a user create a new transaction with a category the amount will be deducted from his budget .
 # TRANSACTION IN CATEGORY FOOD AMOUNT 500
 # GO TO BUDGET AND GET THE USERS INFO THEN GO TO THE CATEGORY FOOD THEN DEDUCT THE AMOUNT FROM THE BUDGET AND GIVE HIM A NOTIFICATION
+
 import mypkg.services.sessions as S
 import mypkg.models.budget as B
 import mypkg.models.transaction as T
@@ -11,10 +12,30 @@ import mypkg.services.file_manager as F
 
 # this will be calculated over a category
 def IncomeVsExpense():
-    data1=F.load_transaction()
-    data2=F.LoadBudget()
-    current_user=S.get_current_user()
-    for key in data2:
-        print(key)
+    category=input("enter your category or choose one :-").strip().upper()
+    Get_Total_categoryTransactions(category=category)
+    print('')
 
-IncomeVsExpense()
+
+
+    # data 2 is budget
+    # data2=TData2[current_user]
+    
+def Get_Total_categoryTransactions(category):
+    current_user=S.get_current_user()
+    TData1=F.load_transaction()
+    TData2=F.LoadBudget()
+        # data1 is transactions
+    data1=TData1[current_user]
+    total=0
+    for transaction_id  in data1:
+        for entry in data1[transaction_id].items():
+            if entry.get("category","")==category:
+                amount= entry.get("amount",0)
+                total+=amount
+                return total
+            else:
+                return None
+
+
+
